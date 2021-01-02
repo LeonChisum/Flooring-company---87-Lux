db9ee216e3400fa500dc01175c04502dddd867d8
LeonChisum <leonchisum3306@gmail.com>
2021-01-02 09:36:55 -0500

    made changes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from 'next/head';
 
 import Footer from '../components/Footer.js';
+import Navbar from '../components/Navbar.js';
+import Header from '../components/Header.js';
 
-import Navbar from '../components/Navbar.js'
-import Header from '../components/Header.js'
-
-import "../styles/style.scss"
+import '../styles/style.scss';
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar/>
-      <div className="container">
-        <Header/>
-      </div>
-      <Footer />
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<div className='container'>
+				<Header />
+			</div>
+			<Footer />
+		</div>
+	);
 }
