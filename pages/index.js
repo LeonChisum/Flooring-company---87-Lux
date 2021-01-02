dd49873788e81cacc6013b5100b7d138df139260
LeonChisum <leonchisum3306@gmail.com>
2021-01-02 07:45:26 -0500

    added footer section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-import Navbar from '../components/AllComponents.js'
+import Navbar from '../components/AllComponents.js';
+import Footer from '../components/Footer.js';
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Footer />
+		</div>
+	);
 }
