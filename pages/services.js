87624bcd73739c99020d43596ebc861fdc583a59
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2021-01-03 12:40:56 -0500

    added a bottompx in line 424

--- pages/services.js
+++ pages/services.js
@@ -23,18 +23,21 @@ const Services = () => {
 			
 			
 		<div>
-		<div className="services">
-	
-			<h1>Services we offer</h1>
+		<div className="lux">
+		
+			<h1>Luxury Vinyl</h1>
+			
 		</div>
 			<div>
 			<br></br>
+			<img className="squarepic"src='./square.png' />
 			
-			<div className="firstp">
-		
-			<p>we offer a wide range of flooring services, and high quality installation services at affordable</p> 
+			<div className="secondp">
 			
-			<p>prices.The wide of services we offer help our customers get what they need, at the highest</p>
+			<p>Luxury vinyl is one of the most durable flooring options out there. it can</p>
+			<p>withstand water and moisture, as well as wear and tear. It is also stain and</p>
+			<p>scratch resistant. This durability makes it an ideal flooring option.</p>
+			
 			<br></br>
 		</div>
 		</div>
