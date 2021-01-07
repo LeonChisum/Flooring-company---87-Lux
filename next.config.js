0b2391be3abb4eabddcf849989c5d6f5ef704a82
Nicksonder Examar <nextgendev12@gmail.com>
2021-01-06 21:37:46 -0500

    Doing the Gallery page

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@ const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 
 module.exports = withCSS(withSass());
+
+module.exports = {
+    images: {
+        devicesSizes: [285, 325, 450, 600, 768, 1024, 1500]
+    }
+}
