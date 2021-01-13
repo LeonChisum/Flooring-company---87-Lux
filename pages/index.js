2d6e248cdc0278776db03b0e0d64864c838dd1b0
LeonChisum <leonchisum3306@gmail.com>
2021-01-13 14:03:29 -0500

    finished mobile view for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import Header from "../components/header";
-import ServicesWeOffer from "../components/servicesWeOffer";
-import Testomonials from "../components/Testomonials";
-import ViewOurProjects from "../components/ViewOurProjects";
-import WhatMore from "../components/WhatMore";
-import WhoAreWe from "../components/WhoAreWe";
-import WhyWorkWithUs from "../components/whyWorkWithUs";
+import Header from '../components/header';
+import ServicesWeOffer from '../components/servicesWeOffer';
+import Testomonials from '../components/Testomonials';
+import ViewOurProjects from '../components/ViewOurProjects';
+import WhatMore from '../components/WhatMore';
+import WhoAreWe from '../components/WhoAreWe';
+import WhyWorkWithUs from '../components/whyWorkWithUs';
 
-import "../styles/style.scss";
+import '../styles/style.scss';
 
 export default function Home(props) {
-  return (
-    <div>
-      <Header />
-      <WhyWorkWithUs />
-      <ServicesWeOffer />
-      <Testomonials />
-      <ViewOurProjects />
-      <WhoAreWe />
-      <WhatMore />
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<meta
+					description='We pride in installing the highest quality, affordable, vinyl floor in the Tampa area.'
+					content='flooring company, flooring, floor, floor covering, lay out, house, renovation, quote, estimate'
+				/>
+				<title>Alexis Flooring - Affordable Floore Installations</title>
+			</Head>
+			<Header />
+			<WhyWorkWithUs />
+			<ServicesWeOffer />
+			<Testomonials />
+			<ViewOurProjects />
+			<WhoAreWe />
+			<WhatMore />
+		</div>
+	);
 }
