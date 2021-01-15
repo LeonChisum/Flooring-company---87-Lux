0b49f3f391290612012832363b6f3d9b8718e1c9
Nicksonder Examar <nextgendev12@gmail.com>
2021-01-14 21:51:10 -0500

    Fixed the favicon

--- pages/gallery.js
+++ pages/gallery.js
@@ -18,24 +18,24 @@ const Gallery = () => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/img/favicon_package/apple-touch-icon.png"
+          href="/favicon_package/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/img/favicon_package/favicon-32x32.png"
+          href="/favicon_package/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/img/favicon_package/favicon-16x16.png"
+          href="/favicon_package/favicon-16x16.png"
         />
-        <link rel="manifest" href="/img/favicon_package/site.webmanifest" />
+        <link rel="manifest" href="/favicon_package/site.webmanifest" />
         <link
           rel="mask-icon"
-          href="/img/favicon_package/safari-pinned-tab.svg"
+          href="/favicon_package/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <meta name="msapplication-TileColor" content="#da532c" />
